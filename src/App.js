3b3b0dca2c8566c7a6270949c0bdc0a9a0c4ba85
Felipe Dias <52228698+xkSilent@users.noreply.github.com>
2022-06-06 23:00:11 -0300

    NavBar criado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Navbar from "./components/nav/nav";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Navbar />
     </div>
   );
 }
